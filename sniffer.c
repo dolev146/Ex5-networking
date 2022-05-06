9dff80e7a0335850d52e30ea96f2741368b81ef7
yakov103 <yaakov103@gmail.com>
2022-05-06 08:45:35 -0700

    snif is working

--- sniffer.c
+++ sniffer.c
@@ -1,57 +1,61 @@
+#include <sys/socket.h>
+#include <stdlib.h>
+
 #include <pcap.h>
 #include <stdio.h>
 #include <arpa/inet.h>
+#include <net/ethernet.h>
 
-/* Ethernet header */
-struct ethheader {
-  u_char  ether_dhost[6]; /* destination host address */
-  u_char  ether_shost[6]; /* source host address */
-  u_short ether_type;     /* protocol type (IP, ARP, RARP, etc) */
+struct ethheader
+{
+  u_char ether_dhost[ETHER_ADDR_LEN]; /* destination host address */
+  u_char ether_shost[ETHER_ADDR_LEN]; /* source host address */
+  u_short ether_type;                 /* IP? ARP? RARP? etc */
 };
 
-/* IP Header */
-struct ipheader {
-  unsigned char      iph_ihl:4, //IP header length
-                     iph_ver:4; //IP version
-  unsigned char      iph_tos; //Type of service
-  unsigned short int iph_len; //IP Packet length (data + header)
-  unsigned short int iph_ident; //Identification
-  unsigned short int iph_flag:3, //Fragmentation flags
-                     iph_offset:13; //Flags offset
-  unsigned char      iph_ttl; //Time to Live
-  unsigned char      iph_protocol; //Protocol type
-  unsigned short int iph_chksum; //IP datagram checksum
-  struct  in_addr    iph_sourceip; //Source IP address
-  struct  in_addr    iph_destip;   //Destination IP address
+struct ipheader
+{
+  unsigned char iph_ihl : 4,       //IP header length
+      iph_ver : 4;                 //IP version
+  unsigned char iph_tos;           //Type of service
+  unsigned short int iph_len;      //IP Packet length (data + header)
+  unsigned short int iph_ident;    //Identification
+  unsigned short int iph_flag : 3, //Fragmentation flags
+      iph_offset : 13;             //Flags offset
+  unsigned char iph_ttl;           //Time to Live
+  unsigned char iph_protocol;      //Protocol type
+  unsigned short int iph_chksum;   //IP datagram checksum
+  struct in_addr iph_sourceip;     //Source IP address
+  struct in_addr iph_destip;       //Destination IP address
+};
+
+struct icmpheader
+{
+  unsigned char icmp_type;        // ICMP message type
+  unsigned char icmp_code;        // Error code
+  unsigned short int icmp_chksum; //Checksum for ICMP Header and data
+  unsigned short int icmp_id;     //Used for identifying request
+  unsigned short int icmp_seq;    //Sequence number
 };
 
 void got_packet(u_char *args, const struct pcap_pkthdr *header,
-                              const u_char *packet)
+                const u_char *packet)
 {
   struct ethheader *eth = (struct ethheader *)packet;
 
-  if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
-    struct ipheader * ip = (struct ipheader *)
-                           (packet + sizeof(struct ethheader)); 
+  if (ntohs(eth->ether_type) == 0x0800)
+  { // 0x0800 is IP type
+    struct ipheader *ip = (struct ipheader *)(packet + sizeof(struct ethheader));
 
-    printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));   
-    printf("         To: %s\n", inet_ntoa(ip->iph_destip));    
+    printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));
+    printf("         To: %s\n", inet_ntoa(ip->iph_destip));
 
-    /* determine protocol */
-    switch(ip->iph_protocol) {                                 
-        case IPPROTO_TCP:
-            printf("   Protocol: TCP\n");
-            return;
-        case IPPROTO_UDP:
-            printf("   Protocol: UDP\n");
-            return;
-        case IPPROTO_ICMP:
-            printf("   Protocol: ICMP\n");
-            return;
-        default:
-            printf("   Protocol: others\n");
-            return;
-    }
+    //getting the pointer to the ICMP part of the packet
+    int ip_header_len = ip->iph_ihl * 4;
+    struct icmpheader *icmp = (struct icmpheader *)(packet + sizeof(struct ethheader) + ip_header_len);
+
+    printf("       Type: %d\n", icmp->icmp_type);
+    printf("       Code: %d\n\n", icmp->icmp_code);
   }
 }
 
@@ -60,19 +64,25 @@ int main()
   pcap_t *handle;
   char errbuf[PCAP_ERRBUF_SIZE];
   struct bpf_program fp;
-  char filter_exp[] = "ip proto icmp";
+  char filter_exp[] = "icmp";
   bpf_u_int32 net;
 
-  // Step 1: Open live pcap session on NIC with name enp0s3
-  handle = pcap_open_live("enp0s3", BUFSIZ, 1, 1000, errbuf);
+  // Step 1: Open live pcap session on NIC with name eth3.
+  // Students need to change "eth3" to the name found on their own
+  // machines (using ifconfig). The interface to the 10.9.0.0/24
+  // network has a prefix "br-" (if the container setup is used).
+  handle = pcap_open_live("ens33", BUFSIZ, 1, 1000, errbuf);
 
   // Step 2: Compile filter_exp into BPF psuedo-code
   pcap_compile(handle, &fp, filter_exp, 0, net);
-  pcap_setfilter(handle, &fp);
-
+  if (pcap_setfilter(handle, &fp) != 0)
+  {
+    pcap_perror(handle, "Error:");
+    exit(EXIT_FAILURE);
+  }
+  
   // Step 3: Capture packets
   pcap_loop(handle, -1, got_packet, NULL);
-
-  pcap_close(handle);   //Close the handle
+  pcap_close(handle); //Close the handle
   return 0;
 }
